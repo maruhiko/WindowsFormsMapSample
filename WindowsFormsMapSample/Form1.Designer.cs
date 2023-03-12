2318630e4ab899b715b255ccc87f344d2beb4d2b
maruhiko <maru_hiko@icould.com>
2023-03-13 05:31:54 +0900

    first commit

---
+++ WindowsFormsMapSample/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsMapSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(1, -1);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomEnabled = true;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(801, 451);
+            this.gMapControl1.TabIndex = 0;
+            this.gMapControl1.Zoom = 0D;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gMapControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsMapSample/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+
+
+namespace WindowsFormsMapSample
+{
+    public partial class Form1 : Form
+    {
+        static int n = 100;
+        List<double> latitudes = new List<double>();
+        List<double> longitudes = new List<double>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 緯度・経度のデータを準備する
+            CreateGPSData();
+
+
+            // 現在位置を取得する
+
+            // GMapControlの設定
+            gMapControl1.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+            gMapControl1.SetPositionByKeywords("東京都");
+            gMapControl1.MinZoom = 1;
+            gMapControl1.MaxZoom = 100;
+            gMapControl1.Zoom = 13;
+
+            // 中心を最初のデータ位置に
+            gMapControl1.Position = new PointLatLng(latitudes.First(), longitudes.First());
+
+            // マーカーを追加する
+            var markerOverlay = new GMapOverlay("markers");
+            for (int i = 0; i < n; i++)
+            {
+                var position = new PointLatLng(latitudes[i], longitudes[i]); // 例として東京駅の緯度経度を指定
+                var marker = new GMap.NET.WindowsForms.Markers.GMarkerGoogle(position, GMap.NET.WindowsForms.Markers.GMarkerGoogleType.red_small);
+                markerOverlay.Markers.Add(marker);
+            }
+
+            gMapControl1.Overlays.Add(markerOverlay);
+        }
+
+        /// <summary>
+        /// サンプル用にグラフに表示するためのデータを作成
+        /// </summary>
+
+        void CreateGPSData()
+        {
+            // データを作る。東京駅からスカイツリーまでのn点を採用する
+            var start = new PointLatLng(35.681167, 139.767052); // 東京駅
+            var end = new PointLatLng(35.710063, 139.810700); // スカイツリー
+
+            // ルート検索を行う
+            var route = GMap.NET.MapProviders.OpenStreetMapProvider.Instance.GetRoute(start, end, false, false, 15);
+
+            // ルートをn点で分割する
+            var step = (double)route.Points.Count / n;
+            for (int i = 0; i < n; i++)
+            {
+                var point = route.Points[(int)(i * step)];
+                latitudes.Add(point.Lat);
+                longitudes.Add(point.Lng);
+            }
+        }
+
+
+        /// ////////////////////////////////////////////////////////////////////////////
+        // 以下、現在位置を表示させるためのプログラム
+        /// ////////////////////////////////////////////////////////////////////////////
+
+        // 現在座標を示すindex
+        int posIndex = 0;
+
+        // 現在位置表示用マーカー/オーバーレイ
+        GMarkerGoogle currentMarker;
+        GMapOverlay currentPosOverlay = new GMapOverlay("current");
+
+
+        /// <summary>
+        /// タイマーが動くごとに次のGPS座標に移動する。
+        /// 現在の座標は青色マーカーで表示
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // 現在位置の更新
+            if (currentMarker != null)
+            {
+                currentPosOverlay.Markers.Remove(currentMarker);
+            }
+
+            PointLatLng currentPosition = new PointLatLng(latitudes[posIndex], longitudes[posIndex]);
+            currentMarker = new GMap.NET.WindowsForms.Markers.GMarkerGoogle(currentPosition, GMap.NET.WindowsForms.Markers.GMarkerGoogleType.blue_small);
+            currentPosOverlay.Markers.Add(currentMarker);
+            gMapControl1.Overlays.Add(currentPosOverlay);
+
+            // 表示データの更新
+            posIndex++;
+            if (posIndex >= n) posIndex = 0;
+        }
+    }
+}
